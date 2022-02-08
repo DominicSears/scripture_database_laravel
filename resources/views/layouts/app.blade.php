1c9bb0827ec48b24f9cc5b02d99097c675dbf320
Dominic Sears <dominicsears@hotmail.com>
2022-02-08 12:11:01 -0800

    Update to Laravel 9

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
@@ -12,9 +12,6 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <style>
-            [x-cloak] { display: none; }
-        </style>
 
         @livewireStyles
 
