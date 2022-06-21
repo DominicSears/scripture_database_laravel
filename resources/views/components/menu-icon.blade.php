c0e44c161b617d1c51fdad356ad2238a8d2f4fab
Dominic Sears <dominicsears@hotmail.com>
2022-06-21 12:03:56 -0700

    Added dropdowns for sidebar

--- resources/views/components/menu-icon.blade.php
+++ resources/views/components/menu-icon.blade.php
@@ -1,15 +1,24 @@
 <div @class([
-    'w-full items-center p-2',
-    'flex flex-row justify-between' => ! $singleItem,
+    'w-full items-start p-2',
+    'flex flex-col hover:cursor-pointer' => ! $singleItem,
     'text-slate-500' => ! $isSelected,
     'bg-sky-100 rounded-lg text-sky-500' => $isSelected
-])>
-    <div class="space-x-4 items-center flex flex-row">
-        {{ $slot }}
+]) {{ $attributes }}>
+    <div class="items-center flex flex-row justify-between w-full">
+        <div class="flex flex-row space-x-4 items-center">
+            {{ $slot }}
+        </div>
+        @if (! $singleItem)
+            <svg name="menu-extend" xmlns="http://www.w3.org/2000/svg" class="menu-extend h-4 w-4 hidden" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+            </svg>
+        @endif
     </div>
-    @if (! $singleItem)
-        <svg name="menu-extend" xmlns="http://www.w3.org/2000/svg" class="menu-extend h-4 w-4 hidden" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-        </svg>
-    @endif
+   @if (isset($children))
+        <div {{ $children->attributes }} x-cloak>
+            <div class="flex flex-col space-y-2 justify-start pt-4 pl-10">
+                {{ $children }}
+            </div>
+        </div>
+   @endif
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
             <!-- Sidebar -->
             <div class="h-full flex flex-col border-r border-slate-200 px-2 w-fit" id="sidebar" x-data="{
                 open: false,
-                openMenu: function () {
+                openMenu() {
                     for (let text of document.getElementsByClassName('icon-name')) {
                         text.style.display = this.open ? 'none' : 'block';
                     }
@@ -52,14 +52,39 @@
 
                     this.open = !this.open;
                 }
-            }">
+            }" x-transition>
                 <!-- Hamburget Menu -->
                 <div class="w-full pt-8 pl-2" x-on:click="openMenu()">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-slate-500 hover:cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
                 </div>
-                <div class="w-full flex flex-col space-y-6 pt-8">
+                <div class="w-full flex flex-col space-y-6 pt-8" x-data="{
+                    usersOpen: false,
+                    groupsOpen: false,
+                    doctrinesOpen: false,
+                    nuggetsOpen: false,
+                    openSub(name) {
+                        if (!open) {
+                            openMenu();
+                        }
+
+                        switch(name) {
+                            case 'users':
+                                this.usersOpen = !this.usersOpen;
+                                break;
+                            case 'groups':
+                                this.groupsOpen = !this.groupsOpen;
+                                break;
+                            case 'doctrines':
+                                this.doctrinesOpen = !this.doctrinesOpen;
+                                break;
+                            case 'nuggets':
+                                this.nuggetsOpen = !this.nuggetsOpen;
+                                break;
+                        }
+                    }
+                }">
                     <!-- Home -->
                     <a href="{{ route('dashboard') }}">
                         <x-menu-icon :isSelected="request()->is('dashboard')" :singleItem="true">
@@ -70,30 +95,45 @@
                         </x-menu-icon>
                     </a>
                     <!-- Users -->
-                    <x-menu-icon :isSelected="request()->is('users*')">
+                    <x-menu-icon :isSelected="request()->is('users*')" x-on:click="openSub('users')">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                         </svg>
                         <p class="font-semibold icon-name" style="display: none;">Users</p>
+                        <x-slot:children x-show="usersOpen">
+                            <p>Edit</p>
+                            <p>Delete</p>
+                        </x-slot:children>
                     </x-menu-icon>
                     <!-- Religions/Denominations -->
-                    <x-menu-icon :isSelected="request()->is('religions*') || request()->is('denominations*')">
+                    <x-menu-icon :isSelected="request()->is('religions*') || request()->is('denominations*')" x-on:click="openSub('groups')">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                         <p class="font-semibold icon-name" style="display: none;">Religions</p>
+                        <x-slot:children x-show="groupsOpen">
+                            <p>Create Religions</p>
+                            <p>Create Denomiations</p>
+                        </x-slot:children>
                     </x-menu-icon>
                     <!-- Doctrines -->
-                    <x-menu-icon :isSelected="request()->is('doctrines*')">
+                    <x-menu-icon :isSelected="request()->is('doctrines*')" x-on:click="openSub('doctrines')">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                         </svg>
                         <p class="font-semibold icon-name" style="display: none;">Doctrines</p>
+                        <x-slot:children x-show="doctrinesOpen">
+                            <p>Create Doctrines</p>
+                            <p>List Doctrines</p>
+                        </x-slot:children>
                     </x-menu-icon>
                     <!-- Nuggets -->
-                    <x-menu-icon :isSelected="request()->is('nuggets*')">
+                    <x-menu-icon :isSelected="request()->is('nuggets*')" x-on:click="openSub('nuggets')">
                         <div class="w-6 h-6 bg-gray-300 rounded-full"></div>
                         <p class="font-semibold icon-name" style="display: none;">Nuggets</p>
+                        <x-slot:children x-show="nuggetsOpen">
+                            <a href="{{ route('nuggets.list') }}" class="text-decoration-none">Show Nuggets</a>
+                        </x-slot:children>
                     </x-menu-icon>
                 </div>
             </div>
