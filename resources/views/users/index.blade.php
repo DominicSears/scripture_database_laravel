fc3f2f14d3e2877fe51b28a71d958753fe42aabd
Dominic Sears <dominicsears@hotmail.com>
2022-06-23 13:04:23 -0700

    Check for null faiths

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="flex flex-col w-full h-full justify-center items-center">
         @forelse ($users as $user)
-            <p>{{ $user->name }} - {{ $user->faith->title }}</p>
+            <p>{{ $user->name }} - {{ $user->faith?->title ?? 'N/A' }}</p>
         @empty
             <p>No users</p>
         @endforelse
