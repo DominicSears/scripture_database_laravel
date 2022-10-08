dfea1a38cd90c8a0f471da2ef67538bcdb8cfa30
Dominic Sears <dominicsears@hotmail.com>
2022-10-08 08:56:07 -0700

    List items component

---
+++ app/Http/Livewire/ListItems.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Traits\MapsModels;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use Livewire\Component;
+
+class ListItems extends Component
+{
+    use MapsModels;
+
+    /** @var class-string $classType */
+    public string $classType;
+
+    /** @var Collection<Model> $items */
+    public Collection $items;
+
+    public function mount()
+    {
+        $this->title ??= str($this->mapToCodeName($this->classType))
+            ->plural();
+
+        $this->items ??= collect();
+    }
+
+    public function render()
+    {
+        return view('livewire.list-items');
+    }
+}
---
+++ resources/views/livewire/list-items.blade.php
@@ -0,0 +1,23 @@
+<div class="flex flex-col space-y-4">
+    <div class="flex flex-row space-x-4 justify-between items-center">
+        <h2 class="font-bold text-2xl text-sky-900">{{ $title }}</h2>
+        <button>
+            +
+        </button>
+    </div>
+    @forelse ($items as $item)
+        <div class="flex flex-row space-x-4 justify-between items-center">
+            <p class="font-semibold text-xl text-black">{{ $item->getAttribute('title') }}</p>
+        </div>
+    @empty
+        <div class="flex items-center justify-center">
+            <span>No items available</span>
+        </div>
+    @endforelse
+
+    @if ($items->count() > 10)
+        <a href="#" class="text-lg font-semibold hover:underline text-sky-700 hover:text-sky-800">
+            <span>See More</span>
+        </a>
+    @endif
+</div>
