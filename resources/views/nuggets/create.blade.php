a38c854a5798b7c922089c23ba0ff57ce16c77fc
Dominic Sears <dominicsears@hotmail.com>
2022-10-28 07:52:51 -0700

    Nuggets > create progress

--- resources/views/nuggets/create.blade.php
+++ resources/views/nuggets/create.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="flex flex-col space-y-4 w-full bg-white shadow-lg rounded-md ring-1 ring-gray-200">
-
+    <div class="flex flex-col space-y-4 w-full bg-white shadow-lg rounded-md ring-1 ring-gray-200 h-96 p-8">
+        <h2 class="text-2xl text-sky-900 font-bold">Create Nugget</h2>
     </div>
 </x-app-layout>
