f5583890c0d57c6c25c5dd71ea8f470f05a83a56
Dominic Sears <dominicsears@hotmail.com>
2022-10-29 07:41:09 -0700

    Update tailwindcss config to fix modal width

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,17 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/wire-elements/modal/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
+    safelist: [
+        {
+            pattern: /max-w-(sm|md|lg|xl|2xl|3xl|4xl|5xl|6xl|7xl)/,
+            variants: ['sm', 'md', 'lg', 'xl', '2xl'],
+        },
+    ],
     theme: {
         extend: {
             fontFamily: {
